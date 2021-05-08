a34237420831a7f68f87befec7329ac3382007da
Reda1997 <reda.bekka@gmail.com>
2021-05-08 07:00:46 +0200

    Ajout du fichier .env,installation de quelques middleware

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const app = express();
+const morgan=require('morgan')
+require('dotenv/config');
 
-app.get('/',(req,res)=>{
-    res.send('Hello Api')
+const api = process.env.API_URL
+//middleware
+app.use(express.json());
+app.use(morgan('tiny'))
+
+app.get(`${api}/products`,(req,res)=>{
+    const product = {
+        id:1,
+        name:"gel",
+        image:'some_url'
+    }
+    res.send(product)
+})
+
+app.post(`${api}/products`,(req,res)=>{
+    const produit = req.body
+    console.log(produit)
+    res.send(produit)
 })
 
 app.listen(3001,()=>{
+    console.log(api)
     console.log("Server is running on  http://localhost:3001");
 })
